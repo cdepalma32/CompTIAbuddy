6dab24c531a7b972f4524e85587f4984ea5c7d7c
Crystal D <crystaldepalma@yahoo.com>
2024-07-31 19:52:28 -0400

    updated models' index.js to include models and worked on activity.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,26 @@
-const User = require("./User");
+const Activity = require('./Activity');
+const Certification = require('./Certification');
+const Chapter = require('./Chapter');
+const Flashcard = require('./Flashcard');
+const Notecard = require('./Notecard');
+const PaymentMethod = require('./paymentMethod');
+const Quiz = require('./Quiz');
+const Transaction = require('./Transaction');
+const User = require('./User');
 
-module.exports = { User };
+// Export all models as an object
+module.exports = {
+  Activity,
+  Certification,
+  Chapter,
+  Flashcard,
+  Notecard,
+  PaymentMethod,
+  Quiz,
+  Transaction,
+  User
+};
+
+// module.exports = { User };
+    // not sure if this update is what we need to the models' index.js -- temporarily commented out, above's module.exports...
+
