71c24da10616a21137636b2b9f2fc1317989e03e
lesley-byte <phosphorescence3@hotmail.com>
2024-08-01 10:13:44 -0400

    Updated models: PaymentMethod, Transaction, and User

---
+++ server/models/Transaction.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+
+const TransactionSchema = new Schema(
+  {
+    stripeTransactionId: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    currency: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    created: {
+      type: Date,
+      required: true,
+    },
+    certificationId: {
+      type: Schema.Types.ObjectId,
+      ref: "Certification",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Transaction = model("Transaction", TransactionSchema);
+module.exports = Transaction;
